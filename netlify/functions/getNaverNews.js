9e80c138aae3e2cef71b4a7b0a011287dde75232
hobaek <wogh104@naver.com>
2025-06-01 12:22:42 +0900

    Update getNaverNews.js

--- netlify/functions/getNaverNews.js
+++ netlify/functions/getNaverNews.js
@@ -4,7 +4,9 @@ exports.handler = async function(event) {
   const query = event.queryStringParameters.query || '마케팅';
   const displayCount = 20;
   const url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURIComponent(query)}&display=${displayCount}&sort=date`;
-
+  const clientId = process.env.NAVER_CLIENT_ID;
+  const clientSecret = process.env.NAVER_CLIENT_SECRET;
+  
   try {
     const res = await fetch(url, {
       headers: {
